12be5dc54f8b48862ce8684af9e822f21d892bae
chithanoori akhila <akhila.chithanoori@zemosolabs.com>
2022-01-16 09:04:57 +0530

    type inference

--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
-function sum(n1:number, n2:number) {
+function sum(n1: number, n2: number) {
     // if(typeof n1 !=='number' || typeof n2!=='number'){
     //     throw new Error('Incorrect input')
     // }
     return n1 + n2;
 }
 
-const number1 = 5;
+let number1: number;
+// number1="hello"; //error
+number1 = 10;
+
 const number2 = 2.8;
 
 const result = sum(number1, number2);
