5ef3cca82933108bae773fab5e5688f30940f547
chithanoori akhila <akhila.chithanoori@zemosolabs.com>
2022-01-17 08:13:22 +0530

    arrays

--- app.ts
+++ app.ts
@@ -9,13 +9,33 @@
 // console.log(person.nickname)//error
 // console.log(person.name) //error
 
-const person : {
-    name:string;
-    age:number
-} = {
-    name:"akhila",
-    age:20
-}
+// const person : {
+//     name:string;
+//     age:number
+// } = {
+//     name:"akhila",
+//     age:20
+// }
 
-console.log(person.age);
-console.log(person.name)
+// console.log(person.age);
+// console.log(person.name)
+
+
+const person  = {
+    name:"akhila",
+    age:20,
+    hobbies:['sports','cooking'],
+    role:[2,'author']
+}
+person.role.push('hello')
+person.role[1]=28;
+for(const x of person.role){
+    console.log(x)
+}
+let favouriteActivities:string[];
+// favouriteActivities = 'sports' //error
+favouriteActivities = ['sports']
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toUpperCase())
+}
